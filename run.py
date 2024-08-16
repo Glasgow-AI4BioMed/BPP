531569840c3317f4de5e43c172ff6705d85e285e
ThreeGold-yxh <634312836@qq.com>
2024-08-16 09:02:31 +0100

    run.py

--- run.py
+++ run.py
@@ -1,21 +1,21 @@
-# from flask import redirect, url_for
+from flask import redirect, url_for
 
-# from app import app
+from app import app
 
 # delete
-from flask import Flask
+# from flask import Flask
 
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+# @app.route('/')
+# def hello():
+#     return "Hello, World!"
 # delete
 
 # app.run(host='0.0.0.0', port=5000, debug=False)
 if __name__ == "__main__":
-    app.run()
-    # app.run(host="0.0.0.0", port=5000, debug=False)
+    # app.run()
+    app.run(host="0.0.0.0", port=5000, debug=False)
     
