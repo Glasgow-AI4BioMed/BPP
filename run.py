1bd02ee8538baaf339c5aea946a85c8c429ed80e
ThreeGold-yxh <634312836@qq.com>
2024-08-16 08:45:58 +0100

    test

--- run.py
+++ run.py
@@ -16,6 +16,6 @@ def hello():
 
 # app.run(host='0.0.0.0', port=5000, debug=False)
 if __name__ == "__main__":
-    # app.run()
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run()
+    # app.run(host="0.0.0.0", port=5000, debug=False)
     
